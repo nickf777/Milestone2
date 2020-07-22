9eeaf66049b3708b6c4a63ef7d2b32886a58b5f9
Nicholas Faller <nickfallerguitar@gmail.com>
2020-07-22 07:16:46 -0400

    Addition of betting and hitting functions

--- Blackjack.py
+++ Blackjack.py
@@ -51,9 +51,66 @@ class Hand:
         for card in self.cards:
             if card.rank == 'Ace':
                 self.aces = self.value - 10
+            else:
+                self.aces = self.value
 
 
     def __str__(self):
         for card in self.cards:
             print(card)
         return 'This hand contains ' + str(len(self.cards)) + ' cards.'
+
+class Chips:
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+    
+    def win_bet(self):
+        return self.total + self.bet
+
+    def lose_bet(self):
+        return self.total - self.bet
+    
+    def __str__(self):
+        return 'You currently have $' + str(self.total) + 'in chips.'
+
+def take_bet(chips):
+    try:
+        bet = int(input('How much would you like to bet?'))
+    except:
+        print('That is not a valid bet. Please input a number indicating how much you would like to bet.')
+        take_bet(chips)
+    chips.bet = bet
+    return bet
+
+def hit(deck,hand):
+    return hand.add_card(deck.pop())
+
+def hit_or_stand(deck,hand):
+    global playing
+    correct_choice = False
+    choices = ['Y', 'N']
+    while correct_choice == False:
+        choice = input('Do you want to hit? (Y/N)').upper()
+        if choice not in choices:
+            print('Sorry that is not a viable choice. Please pick Y/N.')
+        else:
+            correct_choice = True
+            break
+    
+    if choice == 'Y':
+        hit(deck,hand)
+        print(f'Your hand contains {hand}')
+    else:
+        playing = False
+        print('You have chosen not to hit.')
+        print(f'Your hand contains {hand} ')
+
+def show_some(player,dealer):
+    #The dealer's first card is visible and all the player's cards are visible. 
+    print(f'The player has {player.hand}')
+    print(f'The dealer is showing {dealer.hand[0]}')
+
+def show_all(player,dealer):
+    #At the end all cards are visible
+    pass
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Milestone2
 Creation of a BlackJack game in Python for the 2nd Milestone Project in the 2020 Complete Python Bootcamp on Udemy
+
+What do I do next:
+    - I want to go through the code and test to make sure the functions are working as intended up to this point
+    - Start building out the game logic immediately to make sure that things are working. Currently it's just too abstract.
+    - Does a player class need to be defined to provide an attribute that distinguishes between player and dealer?
